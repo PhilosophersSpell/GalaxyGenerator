4c952fedf69836daa21adb1eddc9894644b6b424
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-02-27 22:40:08 +0900

    [Randomess] - 균일한 random 이 아니라 중앙에 많이 모여있는데 조금씩 튀어나온 느낌을 주려한다. power 를 써서 소수인 랜덤 값에 지수를 적용해서 작은 값을 훨씬 더 작게, 1에 가까운 값을 본래 값에 가깝게 만들어준다. 그렇게 큰 값은 몰려있고, 작은 값은 몇 없게 해줄 수 있다.

--- src/script.js
+++ src/script.js
@@ -23,6 +23,8 @@ parameters.size = 0.01;
 parameters.radius = 5;
 parameters.branches = 3;
 parameters.spin = 1;
+parameters.randomness = 0.2;
+parameters.randomnessPower = 3;
 
 let geometry = null;
 let material = null;
@@ -50,13 +52,17 @@ const generateGalaxy = () => {
         const spinAngle = radius * parameters.spin;
         const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2;
 
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1); // 확률 반반으로 양수/음수 정해준다.
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+
         const x = 0;
         const y = 1;
         const z = 2;
 
-        positions[i3 + x] = Math.cos(branchAngle + spinAngle) * radius;
-        positions[i3 + y] = 0;
-        positions[i3 + z] = Math.sin(branchAngle + spinAngle) * radius;
+        positions[i3 + x] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+        positions[i3 + y] = randomY;
+        positions[i3 + z] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
     }
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
@@ -85,6 +91,8 @@ gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(gener
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy);
 gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy);
 
 /**
  * Sizes
